0fb5ace884c3a6419206d5b742cbb24a47fb950c
Arvind <arvind687@gmail.com>
2016-09-03 11:31:06 +0530

    Cleaned up code

--- knapsack.cpp
+++ knapsack.cpp
@@ -18,18 +18,19 @@ int knap(int cap) {
 	int n, space, max, t;
 	max = 0;
 	n = 4;
+	if (computedCaps[cap] != -1) return computedCaps[cap];
 	for(int i = 0; i < n; i++)
 		if ((space = cap - items[i].size) >= 0){
-			if (!computedCaps[space]) computedCaps[space] = knap(space);
-			if ((t = computedCaps[space] + items[i].val) > max)
+			if ((t = knap(space) + items[i].val) > max)
 				max = t;
 		}
+	computedCaps[cap] = max;
 	return max;
 }
 
 int main()
 {
-	for(int i = 0; i < N; i++) computedCaps[i] = 0;
+	for(int i = 0; i < N; i++) computedCaps[i] = -1;
 	cout << knap(12);
 	cout << '\n';
 	return 0;
