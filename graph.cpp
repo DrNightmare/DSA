165ee442e73c1427211ba96a5627aaf487c27e47
Arvind <arvind687@gmail.com>
2016-09-07 22:24:45 +0530

    Added BFS and fixed DFS implementation

--- graph.cpp
+++ graph.cpp
@@ -1,31 +1,57 @@
 #include "iostream"
 #include "vector"
 #include "stack"
+#include "queue"
 
 #define REP(i, n) for(int i = 0; i < n; i++)
 
 using namespace std;
 
 vector<int> adj[10];
-bool visited[10];
+bool visitedDFS[10];
+bool visitedBFS[10];
 
+// stack 1 2
+// x 0
 void dfs(int start) {
 	stack<int> s;
 	s.push(start);
 
-	visited[start] = true;
-	cout << "Visited : " << start << '\n';
+	visitedDFS[start] = true;
 	int x;
 
 	while(!s.empty()) {
 		x = s.top();
+		cout << "Visited : " << x << '\n';
 		s.pop();
 		for(int i = 0; i < adj[x].size(); i++) {
 			int neighbor = adj[x][i];
-			if (!visited[neighbor]) {
+			if (!visitedDFS[neighbor]) {
 				s.push(neighbor);
-				visited[neighbor] = true;	
-				cout << "Visited : " << neighbor << '\n';
+				visitedDFS[neighbor] = true;	
+				// cout << "Visited : " << neighbor << '\n';
+			}
+		}
+	}
+}
+
+void bfs(int start) {
+	queue<int> q;
+	q.push(start);
+
+	visitedBFS[start] = true;
+	int x;
+
+	while(!q.empty()) {
+		x = q.front();
+		cout << "Visited : " << x << '\n';
+		q.pop();
+		for(int i = 0; i < adj[x].size(); i++) {
+			int neighbor = adj[x][i];
+			if (!visitedBFS[neighbor]) {
+				q.push(neighbor);
+				visitedBFS[neighbor] = true;	
+				// cout << "Visited : " << neighbor << '\n';
 			}
 		}
 	}
@@ -43,12 +69,15 @@ int main()
 		adj[x].push_back(y);
 	}	
 
-	for(int i = 0; i < nodes; i++) {
-		cout << "Adjacency list of node : " << i << '\n';
-		for(int j = 0; j < adj[i].size(); j++) cout << adj[i][j] << ' ';
-		cout << '\n';
-	}
+	// for(int i = 0; i < nodes; i++) {
+	// 	cout << "Adjacency list of node : " << i << '\n';
+	// 	for(int j = 0; j < adj[i].size(); j++) cout << adj[i][j] << ' ';
+	// 	cout << '\n';
+	// }
 
+	cout << "DFS : \n";
 	dfs(0);
+	cout << "BFS : \n";
+	bfs(0);
 	return 0;
 }
